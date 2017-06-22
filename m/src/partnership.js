0593113a4cd8a6303dbaebab5a0ea31b37965375
Andrii Kornytskyi <akornytskyi@brainence.com>
2017-06-22 16:10:22 +0300

    ui fixes, added mobile version

---
+++ m/src/partnership.js
@@ -0,0 +1,17 @@
+﻿angular.module('brainenceApp').controller("partnershipCtrl", function ($scope, $timeout) {
+
+    $scope.ActivateMenu = function (index) {
+        $(".partnership-menu-item").removeClass('partnership-menu-item-active');
+        document.getElementsByClassName('partnership-menu-item')[index].className += ' partnership-menu-item-active';
+    }
+
+    $scope.showallianceCooperation = function () {
+        $("#allianceCooperationContent").show();
+        $("#directCooperationContent").hide();
+    }
+    $scope.showDirectCooperation = function () {
+        $("#allianceCooperationContent").hide();
+        $("#directCooperationContent").show();
+    }
+
+});
---
+++ m/src/testimonial.js
@@ -0,0 +1,47 @@
+﻿
+angular.module('brainenceApp').controller("testimonialCtrl", function ($scope, $timeout) {
+    var $testimonial_frame = $('#testimonial-frame');
+    jQuery(function ($) {
+        (function () {
+
+            var $testimonial_wrap = $testimonial_frame.parent();
+
+            $testimonial_frame.sly({
+                horizontal: 1,
+                itemNav: 'forceCentered',
+                smart: 1,
+                activateMiddle: 1,
+                mouseDragging: 1,
+                touchDragging: 1,
+                releaseSwing: 1,
+                startAt: 0,
+                scrollBar: $testimonial_wrap.find('.testimonial-scrollbar'),
+                scrollBy: 1,
+                speed: 1000,
+                elasticBounds: 1,
+                easing: 'swing',
+                dragHandle: 1,
+                dynamicHandle: 1,
+                clickBar: 1,
+                scrollTrap: 1
+            }).init();
+        }());
+    });
+
+    $.getJSON("content/testimonials.json", function (data) {
+        $scope.testimonials = data;
+        $scope.$apply();
+        $timeout(function () {
+            $testimonial_frame.sly('reload');
+        }, 100);
+    });
+
+
+    $scope.PrevTesimonial = function () {
+        $testimonial_frame.sly('prev');
+    };
+
+    $scope.NextTesimonial = function () {
+        $testimonial_frame.sly('next');
+    };
+});
