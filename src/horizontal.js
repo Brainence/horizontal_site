ba7edb959b87ffe022bdc9792d88d33cf56725c1
Andrii Kornytskyi <akornytskyi@brainence.com>
2017-06-23 17:53:01 +0300

    change logo

--- src/horizontal.js
+++ src/horizontal.js
@@ -39,12 +39,12 @@ var activatePageNr = function (index) {
     $frame.sly('activate', index);
     clearmenuclass();
     $("#menu-button-" + index).addClass('menu-active');
-    $("#bullet-" + index).addClass('bullet-active');
+    //$("#bullet-" + index).addClass('bullet-active');
 }
 
 var clearmenuclass = function() {
     for (i = 0; i < 6; i++) {
         $("#menu-button-" + i).removeClass('menu-active');
-        $("#bullet-" + i).removeClass('bullet-active');
+        //$("#bullet-" + i).removeClass('bullet-active');
     }
 }
