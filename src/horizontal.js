41e8663a6a7fa1c7e17d1008136e294859b63183
Andrii Kornytskyi <akornytskyi@brainence.com>
2017-06-28 12:12:19 +0300

    fixed pages navigation

--- src/horizontal.js
+++ src/horizontal.js
@@ -2,10 +2,10 @@
 var $frame = $('#oneperframe');
 jQuery(function ($) {
     (function () {
-        
+
         var $wrap = $frame.parent();
 
-            $frame.sly({
+        $frame.sly({
             horizontal: 1,
             itemNav: 'forceCentered',
             smart: 1,
@@ -22,48 +22,53 @@ jQuery(function ($) {
             dragHandle: 1,
             dynamicHandle: 1,
             clickBar: 1
-            }, {
-                change: changePage
-            }).init();
+        }, {
+            change: changePage
+        }).init();
     }());
 });
 
 
+var status = 0; //0-uninizialize, 1-menu button click or onpopstate
+
 
 function changePage(eventName) {
-    var url;
-    activatePageNr(this.rel.activeItem);
-    switch(this.rel.activeItem) {
-        case 0: url = 'home'; break;
-        case 1: url = 'portfolio'; break;
-        case 2: url = 'testimonials'; break;
-        case 3: url = 'contacts'; break;
-        case 4: url = 'career'; break;
-        case 5: url = 'partnership'; break;
+    if (status == 0) {
+        var url;
+        switch (this.rel.activeItem) {
+            case 0: url = 'home'; break;
+            case 1: url = 'portfolio'; break;
+            case 2: url = 'testimonials'; break;
+            case 3: url = 'contacts'; break;
+            case 4: url = 'career'; break;
+            case 5: url = 'partnership'; break;
+        }
+        activatePageNr(this.rel.activeItem);
+        window.history.pushState(url, 'Brainence', "#" + url);
+        switch (url) {
+            case 'home': $('#oneperframe').sly('activate', 0); break;
+            case 'portfolio': $('#oneperframe').sly('activate', 1); break;
+            case 'testimonials': $('#oneperframe').sly('activate', 2); break;
+            case 'contacts': $('#oneperframe').sly('activate', 3); break;
+            case 'career': $('#oneperframe').sly('activate', 4); break;
+            case 'partnership': $('#oneperframe').sly('activate', 5); break;
+            default: $('#oneperframe').sly('activate', 0);
+        }
     }
-    window.history.pushState(null, null, "#" + url);
-    switch (url) {
-        case 'home': $('#oneperframe').sly('activate', 0); break;
-        case 'portfolio': $('#oneperframe').sly('activate', 1); break;
-        case 'testimonials': $('#oneperframe').sly('activate', 2); break;
-        case 'contacts': $('#oneperframe').sly('activate', 3); break;
-        case 'career': $('#oneperframe').sly('activate', 4); break;
-        case 'partnership': $('#oneperframe').sly('activate', 5); break;
-        default: $('#oneperframe').sly('activate', 0);
-    }
+    status = 0;
 }
 
 
 var activatePageNr = function (index) {
-    $frame.sly('activate', index);
+    status = 1;
+    //$frame.sly('activate', index);
     clearmenuclass();
     $("#menu-button-" + index).addClass('menu-active');
-    //$("#bullet-" + index).addClass('bullet-active');
+    status = 0;
 }
 
-var clearmenuclass = function() {
+var clearmenuclass = function () {
     for (i = 0; i < 6; i++) {
         $("#menu-button-" + i).removeClass('menu-active');
-        //$("#bullet-" + i).removeClass('bullet-active');
     }
 }
