fb9181caea68f9cb4c60e2822d0a0b3be7908e9c
Andrii Kornytskyi <akornytskyi@brainence.com>
2017-07-13 12:22:24 +0300

    responsive design, loading bootstrap carousel, changing mobile content

--- src/photos.js
+++ src/photos.js
@@ -2,13 +2,17 @@
 
 
 
-angular.module('brainenceApp').controller("photosCtrl", function ($scope, $timeout) {
-   
-    $.getJSON("content/photos.txt", function (data) {
-        $("#photoCarousel").carousel("pause").removeData();
-        $scope.photos = data;
-        $scope.$apply();
-        $("#photoCarousel").carousel(0);
-    });
+angular.module('brainenceApp').controller("photosCtrl", function ($scope, $rootScope) {
 
+    $rootScope.LoadPhotos = function (index) {
+        $.getJSON("content/photos_full.txt",
+            function (data) {
+                $('#firstBootstrapImage').remove();
+                $("#photoCarousel").carousel("pause").removeData();
+                $scope.photos = data;
+                $scope.$apply();
+                $("#photoCarousel").carousel(index);
+                document.getElementsByClassName('carousel-photo-item')[index].className += ' active';
+            });
+    }
 });
